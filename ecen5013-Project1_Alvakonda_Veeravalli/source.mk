4b21051fe243ed3ad5219d66a1682df41bd700b8
Srivishnu Alvakonda <sral4054@colorado.edu>
2016-09-19 15:36:55 -0600

    Adding source.mk with comments

--- ecen5013-Project1_Alvakonda_Veeravalli/source.mk
+++ ecen5013-Project1_Alvakonda_Veeravalli/source.mk
@@ -1,8 +1,11 @@
+//All file listings
 SOURCE_FILES = main.c project_1_report.c memory.c data.c
 PRE_FILES = main.i  project_1_report.i memory.i data.i
 ASM_FILES = main.s  project_1_report.s memory.s data.s
 OBJECT_FILES = main.o  project_1_report.o memory.o data.o
 
+
+// providing path to the source and header folders/directories
 vpath %.c ./source
 vpath %.h ./header
 
