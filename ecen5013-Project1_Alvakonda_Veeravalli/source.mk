00fe4899bea9b2104ce577c1e1234c9ac5435433
Srivishnu Alvakonda <sral4054@colorado.edu>
2016-09-19 15:32:57 -0600

    Adding source.mk

---
+++ ecen5013-Project1_Alvakonda_Veeravalli/source.mk
@@ -0,0 +1,8 @@
+SOURCE_FILES = main.c project_1_report.c memory.c data.c
+PRE_FILES = main.i  project_1_report.i memory.i data.i
+ASM_FILES = main.s  project_1_report.s memory.s data.s
+OBJECT_FILES = main.o  project_1_report.o memory.o data.o
+
+vpath %.c ./source
+vpath %.h ./header
+
