a9e690ffa68a95a49a1310ab443352ca611be018
Prithvi3141 <Prithvi.Veeravalli@colorado.edu>
2016-12-07 23:24:06 -0700

    Update to i2c.h

--- ecen5013-Project4_Alvakonda_Veeravalli/Luminosity/include/i2c.h
+++ ecen5013-Project4_Alvakonda_Veeravalli/Luminosity/include/i2c.h
@@ -5,26 +5,34 @@
  *      Author: Prithvi
  */
 
-#ifndef INCLUDES_I2C_H_
-#define INCLUDES_I2C_H_
+#ifndef SOURCES_I2C_H_
+#define SOURCES_I2C_H_
 
-#define slave_address_shift   0x01
-#define master_read  0x01
-#define master_write 0x00
+#define WRITE                   0x00  /* Master write  */
+#define READ                    0x01  /* Master read */
+#define I2C_NACK()             I2C0_C1 |= I2C_C1_TXAK_MASK/*for sending NACK*/
+#define I2C_ACK()              I2C0_C1 &= ~I2C_C1_TXAK_MASK/*for sending ACK*/
+#define I2C_RS()               I2C0_C1 |= I2C_C1_RSTA_MASK/* for repeated start*/
+#define I2C_Rx()               I2C0_C1 &= ~I2C_C1_TX_MASK/*I2C in RX mode*/
+#define I2C_WRITE(data)        I2C0_D = data/*write to the data register*/
+/*I2C in Tx Mode*/
+#define I2C_Start()            I2C0_C1 |= I2C_C1_TX_MASK;\
+                               I2C0_C1 |= I2C_C1_MST_MASK
 
-#define I2C_ReStart() I2C0_C1 |= I2C_C1_RSTA_MASK
-#define I2C_Start()   I2C0_C1 |= I2C_C1_MST_MASK
-#define I2C_Stop()    I2C0_C1 &= ~I2C_C1_MST_MASK
-#define I2C_Set_TX_Mode() I2C0_C1 |= I2C_C1_TX_MASK
-#define I2C_Set_RX_Mode() I2C0_C1 &= ~I2C_C1_TXAK_MASK;\
-                          I2C0_C1 &= ~I2C_C1_TX_MASK
+#define I2C_Stop()             I2C0_C1 &= ~I2C_C1_MST_MASK;\
+                               I2C0_C1 &= ~I2C_C1_TX_MASK
 
-void I2C_Clock_Configure();
-void I2C_GPIO_Setup();
-void I2C_Init();
-void I2C_Write_Byte(uint8_t data);
-uint8_t I2C_Read_Byte();
-uint8_t I2C_Read_Register(uint8_t slave_address, uint8_t register_address);
-void I2C_Write_Register(uint8_t slave_address, uint8_t register_address, uint8_t byte);
+#define I2C_Wait()             while((I2C0_S & I2C_S_IICIF_MASK)==0) {} \
+                               I2C0_S |= I2C_S_IICIF_MASK;/*wait till pending interrupts are cleared*/
 
-#endif /* INCLUDES_I2C_H_ */
+void Init_I2C(void);
+void I2C_StartTx(char SlaveID, char Mode);
+void I2CWriteRegister(char SlaveID, char RegisterAddress, char Data);
+char I2CReadRegister(char SlaveID, char RegisterAddress);
+void I2CReadMultiRegisters(char SlaveID, char RegisterAddress, char n, char * r);
+
+
+
+
+#endif /* SOURCES_I2C_H_ */
+
