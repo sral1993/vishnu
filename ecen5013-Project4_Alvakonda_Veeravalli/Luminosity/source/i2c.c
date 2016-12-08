c6552a84a8c2d14573f76d3e22f0d757f940f3f8
Prithvi3141 <Prithvi.Veeravalli@colorado.edu>
2016-12-07 23:25:08 -0700

    Update to i2c.c

--- ecen5013-Project4_Alvakonda_Veeravalli/Luminosity/source/i2c.c
+++ ecen5013-Project4_Alvakonda_Veeravalli/Luminosity/source/i2c.c
@@ -5,89 +5,116 @@
  *      Author: Prithvi
  */
 
-#include "MKL25Z4.h"
-#include "stdint.h"
+/*
+ i2c.c
+ *
+ *  Created on: Dec 7, 2016
+ *      Author: Prithvi
+ */
 #include "i2c.h"
-#include "TSL2561.h"
-
-void I2C_Clock_Configure()
+#include "MKL25Z4.h"
+void Init_I2C(void)
 {
-	SIM_SCGC4 |= SIM_SCGC4_I2C0_MASK;
-	SIM_SCGC5 |= SIM_SCGC5_PORTB_MASK;
-	SIM_SCGC5 |= SIM_SCGC5_PORTA_MASK;
+  SIM_SCGC5 = SIM_SCGC5_PORTA_MASK | SIM_SCGC5_PORTB_MASK|SIM_SCGC5_PORTD_MASK;// Turn on clock to PortA and Port E module
+  SIM_SCGC4 |= SIM_SCGC4_I2C0_MASK;  // Turn on clock to I2C0 module
+  PORTB_PCR0 = PORT_PCR_MUX(2);  // PTE24 pin is I2C0 SCL line
+  PORTB_PCR1 = PORT_PCR_MUX(2);  // PTE25 pin is I2C0 SDA line
+  /*
+  	     	 * I2C0 is clocked by the bus clock, that is core/2.
+  	     	 * For the MMA8451Q inertial sensor on the FRDM-25KLZ board the
+  	     	 * maximum SCL frequency is 400 kHz.
+  	     	 * Assuming PEE mode with core=48MHz, 400 kHz = 48MHz/2 / 60,
+  	     	 * which means a prescaler (SCL divider) of 60.
+  	     	 * According to table 38-41, I2C divider and hold values,
+  	     	 * the closest SCL diver is 64 (375 kHz SCL), which is ICR value 0x12.
+  	     	 * Alternatively, the SCL divider of 30 can be used (ICR=0x05) in combination
+  	     	 * with a multiplicator of 2 (MULT=0x01).
+  	     	 * A note states that ICR values lower than 0x10 might result in a varying
+  	     	 * SCL divider (+/- 4). However the data sheet does not state anything
+  	     	 * useful about that.
+  	     	 */
+  	     I2C0_F = I2C_F_MULT(0x00) | I2C_F_ICR(0x12); /* divide by 64 instead, so 375 kHz */
+  I2C0_C1 = I2C_C1_IICEN_MASK;  /* enable the I2C module */
+  PORTB_PCR8 |= (0|PORT_PCR_ISF_MASK|     // Clear the interrupt flag
+  	     	                       PORT_PCR_MUX(0x1)|     // PTA14 is configured as GPIO
+  	     	                       PORT_PCR_IRQC(0xA));   // PTA14 is configured for falling edge interrupts
+
+  	     	        //Enable PORTA interrupt on NVIC
+  	     	    __enable_irq();
+  	     	   NVIC_EnableIRQ(PORTA_IRQn);
 }
-void I2C_GPIO_Setup()
+void I2C_StartTx (char SlaveID, char Mode)
 {
-	PORTB_PCR0 |= PORT_PCR_MUX(2);
-	PORTB_PCR1 |= PORT_PCR_MUX(2);
-	PORTB_PCR3 |= PORT_PCR_MUX(1)|PORT_PCR_IRQC(0x1010);
-}
-
-void I2C_Init()
-{
-	I2C_Clock_Configure();
-	I2C_GPIO_Setup();
-	I2C0_F |= I2C_F_MULT(0)|I2C_F_ICR(0x1F);
-	I2C0_C1 |= I2C_C1_IICEN_MASK;
-}
-
-void I2C_Write_Byte(uint8_t data)
-{
-	I2C0_D = data;
-	while((I2C0_S & I2C_S_IICIF_MASK)==0);
-	I2C0_S |= I2C_S_IICIF_MASK;
-}
-
-uint8_t I2C_Read_Byte()
-{
-	uint8_t received_byte;
-	ADC0_Reg_Low=I2C0_D;
-
-	while((I2C0_S & I2C_S_IICIF_MASK)==0);
-	I2C0_S |= I2C_S_IICIF_MASK;
-
-	I2C0_C1 |= I2C_C1_TXAK_MASK;
-	ADC0_Reg_Low=I2C0_D;
-
-	while((I2C0_S & I2C_S_IICIF_MASK)==0);
-	I2C0_S |= I2C_S_IICIF_MASK;
-	I2C0_C1 &= ~I2C_C1_MST_MASK;
-
-	ADC0_Reg_High = I2C0_D;
-	return received_byte;
+  SlaveID = SlaveID << 1;
+  SlaveID |= (Mode & 0x01);
+  I2C_Start();
+  I2C_WRITE(SlaveID);
 }
 
-uint8_t I2C_Read_Register(uint8_t slave_address, uint8_t register_address)
-{
-	uint8_t received_byte;
-
-	I2C_Set_TX_Mode();
-	I2C_Start();
-
-	I2C_Write_Byte((slave_address<<slave_address_shift)|master_write);
-
-	I2C_Write_Byte(register_address);
-
-	I2C_ReStart();
-
-	I2C_Write_Byte((slave_address<<slave_address_shift)|master_read);
-
-	I2C_Set_RX_Mode();
-
-	received_byte = I2C_Read_Byte();
-	return received_byte;
+void Pause(void){
+    int n;
+    for(n=1;n<50;n++) {
+    }
 }
-void I2C_Write_Register(uint8_t slave_address, uint8_t register_address, uint8_t byte)
+char I2CReadRegister(char SlaveID, char RegisterAddress)
 {
-	I2C_Set_TX_Mode();
-	I2C_Start();
+  char result;
 
-	I2C_Write_Byte((slave_address<<slave_address_shift)|master_write);
+  I2C_StartTx(SlaveID,WRITE);
+  I2C_Wait();
+  I2C_WRITE(RegisterAddress);
+  I2C_Wait();
+  I2C_RS();
+  I2C_WRITE((SlaveID << 1) | READ);
+  I2C_Wait();
+  I2C_Rx();
+  I2C_NACK();// Send NACK to tell dat dis is last transmission
+  result = I2C0_D ; //In master receive mode, reading this register
+                       // initiates receiving of the next byte of data.
+  I2C_Wait();
+  I2C_Stop(); // Send Stop
+  result = I2C0_D ; // reading the register
+  Pause();
+  return result;
+}
 
-	I2C_Write_Byte(register_address);
+void I2CWriteRegister(char SlaveID, char RegisterAddress, char Data)
+{
+  I2C_StartTx(SlaveID,WRITE);
+  I2C_Wait();
+  I2C_WRITE(RegisterAddress);
+  I2C_Wait();
+  I2C_WRITE(Data);
+  I2C_Wait();
+  I2C_Stop();
+  Pause();
+}
+void I2CReadMultiRegisters(char SlaveID, char RegisterAddress, char n, char * r)
+{
+  char i;
 
-	I2C_Write_Byte(byte);
-
-	I2C_Stop();
-    I2C0_C1 &= ~I2C_C1_TX_MASK;
+  I2C_StartTx(SlaveID,WRITE);
+  I2C_Wait();
+  I2C_WRITE(RegisterAddress);
+  I2C_Wait();
+  I2C_RS();
+  I2C_WRITE((SlaveID << 1) | 0x01);
+  I2C_Wait();
+  I2C_Rx();
+  I2C_ACK();
+  i = I2C0_D;
+  for(i=0;i<n-2;i++)
+  {
+    *r = I2C0_D;
+    r++;
+    I2C_Wait();
+  }
+  I2C_NACK();
+  *r = I2C0_D;
+  r++;
+  I2C_Wait();
+  I2C_Stop();
+  *r = I2C0_D;
+  r--;
+  Pause();
 }
