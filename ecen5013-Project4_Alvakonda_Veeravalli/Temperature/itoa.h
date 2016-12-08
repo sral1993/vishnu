de6a0e66e264d156057eef22a5609abb8f8659e4
Prithvi3141 <Prithvi.Veeravalli@colorado.edu>
2016-12-07 23:50:50 -0700

    commiting itoa.h

---
+++ ecen5013-Project4_Alvakonda_Veeravalli/Temperature/itoa.h
@@ -0,0 +1,15 @@
+/*
+ * itoa.h
+ *
+ *  Created on: Nov 25, 2016
+ *      Author: Prithvi
+ */
+#ifndef SOURCES_ITOA_H_
+#define SOURCES_ITOA_H_
+#include <stdint.h>
+int8_t* my_itoa(int8_t *str,int32_t data,int32_t base);
+
+
+
+
+#endif /* SOURCES_ITOA_H_ */
