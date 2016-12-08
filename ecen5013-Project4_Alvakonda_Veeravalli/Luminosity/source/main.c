2c4d1600dbd4506d55f6d04131407010df16e2bb
Prithvi3141 <Prithvi.Veeravalli@colorado.edu>
2016-12-07 23:26:57 -0700

    Update to main.c

--- ecen5013-Project4_Alvakonda_Veeravalli/Luminosity/source/main.c
+++ ecen5013-Project4_Alvakonda_Veeravalli/Luminosity/source/main.c
@@ -5,21 +5,50 @@
  *      Author: Prithvi
  */
 
+/*
+ * main.c
+ *
+ *  Created on: Nov 25, 2016
+ *      Author: Prithvi
+ */
+
 #include "MKL25Z4.h"
 #include "stdint.h"
 #include "i2c.h"
 #include "TSL2561.h"
+#include "led.h"
 
 static int i = 0;
 
 int main(void)
 {
-	I2C_Init();
+	uint8_t ambience_low;
+	uint8_t ambience_high;
+	uint16_t ambience;
+	uint8_t dummy;
+	reading = &dummy;
+	Init_I2C();
 	TSL2561_Init();
 	TSL2561_GPIO_Int_Enable();
-	while(1);
-//	I2C_Read_Register(TSL2561_slaveaddress, TSL2561_ADC0_Low_Reg );
+	while(1)
+	{
+	    I2CReadMultiRegisters(TSL2561_slaveaddress, TSL2561_ADC0_Low_Reg, 2, reading);
+        ambience_low = *reading;
+        reading++;
+        ambience_high = *reading;
+        ambience = ambience_high*256 + ambience_low;
+        if(ambience < 0x000F)
+        {
+        	LED_RED(20);
+        }
+        else if(ambience > 0x0800)
+        {
+        	LED_RED(0);
+        }
+	}
+
 }
 ////////////////////////////////////////////////////////////////////////////////
 // EOF
 ////////////////////////////////////////////////////////////////////////////////
+
