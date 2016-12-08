78c8d455ac39319ee16b9cd21c0ab67e50c3d8a2
sral1993 <sral4054@colorado.edu>
2016-12-07 22:03:48 -0700

    creating led.h

---
+++ ecen5013-Project4_Alvakonda_Veeravalli/Accelerometer/led.h
@@ -0,0 +1,26 @@
+/*
+ * led.h
+ *
+ *  Created on: Dec 5, 2016
+ *      Author: Vishnu
+ */
+
+#ifndef SOURCES_LED_H_
+#define SOURCES_LED_H_
+
+void TPM0_Setup(int mod_value);
+void TPM2_Setup(int mod_value);
+void Blue(int mod_value);
+void Red(int mod_value);
+void Green(int mod_value);
+void Yellow(int mod_value);
+void violet(int mod_value);
+void cyan(int mod_value);
+void white(int mod_value);
+void Set_Brightness();
+void off(int mod_value);
+
+
+
+
+#endif /* SOURCES_LED_H_ */
